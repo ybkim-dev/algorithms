809b79a5a7f62f7cd4fd167ae72b1bc27b04eaee
ybkim-dev <beanybigggy@gmail.com>
2022-01-20 23:57:40 +0900

    update dfs/bfs algorithm 미로 탈출 - 오류 수정

--- DFSBFS/미로 탈출.py
+++ DFSBFS/미로 탈출.py
@@ -11,27 +11,24 @@ visited = [[False] * m for _ in range(n)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-count = 1
+
 def bfs(x, y):
     global count
     queue = deque()
     queue.append([0,0])
-    graph[0][0] = count
     visited[0][0] = True
     while queue:
         v = queue.popleft()
         # queue가 비었을 때 --> 1개 phase가 끝났을 때 count += 1
-        if len(queue) == 0:
-            count += 1
         for i in range(4):
             nx = v[0] + dx[i]
             ny = v[1] + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             else:
-                if visited[nx][ny] == False and graph[nx][ny] != 0:
+                if visited[nx][ny] == False and graph[nx][ny] == 1:
                     queue.append([nx, ny])
-                    graph[nx][ny] = count
+                    graph[nx][ny] = graph[v[0]][v[1]] + 1
                     visited[nx][ny] = True
 
 
