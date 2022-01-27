58a37f6fd9e821bcfc7ec6009f0c460544461109
ybkim-dev <beanybigggy@gmail.com>
2022-01-27 14:40:31 +0900

    add dynamic programming algorithm fibonacci top-down & bottom-up

--- dynamic programming/fibonacci.py
+++ dynamic programming/fibonacci.py
@@ -1,6 +1,8 @@
 # memoization list.
 d = [0] * 100
+d_bottom_up = [0] * 100
 
+# top-down (재귀 방식)
 def fibo(x):
     if x == 1 or x == 2:
         return 1
@@ -9,5 +11,15 @@ def fibo(x):
     else:
         d[x] = fibo(x-1) + fibo(x-2)
         return d[x]
+
+# bottom-up (for-loop 방식)
+d_bottom_up[1] = 1
+d_bottom_up[2] = 1
+n = 99
+
+for i in range(3,n+1):
+    d_bottom_up[i] = d_bottom_up[i-2] + d_bottom_up[i-1]
+
+print(d_bottom_up[n])
 
 print(fibo(99))
