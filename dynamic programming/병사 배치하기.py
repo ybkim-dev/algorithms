a19af831d1f6267054970e3f9f8d139ec4ad93c9
ybkim-dev <beanybigggy@gmail.com>
2022-02-03 13:52:13 +0900

    [feat] : add dynamic programming algorithm 병사 배치하기 using LIS algorithm

---
+++ dynamic programming/병사 배치하기.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+array = list(map(int, input().split()))
+
+#LIS 적용을 위해 reverse
+array.reverse()
+
+dp = [1] * n
+
+for i in range(1,n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
